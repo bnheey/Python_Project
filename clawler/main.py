83c4467fbfff0e443b1d09ec2203219b005800e3
Heeeon <bbjoite09@naver.com>
2021-04-23 00:51:58 +0900

    MODIFY main 21/04/23 00:51

--- clawler/main.py
+++ clawler/main.py
@@ -4,10 +4,17 @@
 pip install request
 pip install beautifulsoup4
 '''
-import requests
+import urllib.request
 from bs4 import BeautifulSoup
+
 
 url = "http://builder.hufs.ac.kr/user/indexSub.action?codyMenuSeq=129898191&siteId=soft&menuType=T&uId=9&sortChar=A&linkUrl=06-3.html&mainFrame=right"
 
-r = requests.get(url)
-print(r.content)
+# 분석 준비
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, "html.parser")
+
+table = soup.find('tbody')
+for t in table:
+    links = t.find('a')
+    print(links)
