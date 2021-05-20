d19a3fb6c3f1eba81e592bdca139f784cde30f7c
Heeeon <bbjoite09@naver.com>
2021-05-21 02:46:14 +0900

    카톡 전송

--- crawling/crawling.py
+++ crawling/crawling.py
@@ -1,7 +1,31 @@
-import pprint
+import json
 
 import requests
 from bs4 import BeautifulSoup
+import time
+import secret
+
+KAKAO_TOKEN = secret.kakao_rest_api
+send_list=[]
+
+def send_to_kakao(text):
+    header = {"Authorization": "Bearer " + KAKAO_TOKEN}
+    url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
+    # url = "https://kapi.kakao.com/v2/api/talk/memo/send?template_id=53949"
+
+
+
+    post = {
+        "object_type": "text",
+        "text": text,
+        "link": {
+            "web_url": "https://developers.kakao.com",
+            "mobile_web_url": "https://developers.kakao.com"
+        },
+    }
+    data = {"template_object": json.dumps(post)}
+
+    return requests.post(url, headers=header, data=data)
 
 
 def get_data():
@@ -19,8 +43,6 @@ def get_data():
 
     # select를 이용해서, tr들을 불러오기
     datas = soup.select('#board-container > div.list > form:nth-child(2) > table > tbody > tr')
-    # print(datas)
-
 
     for data in datas:
         # data 안에 a 가 있으면,
@@ -30,9 +52,28 @@ def get_data():
 
         date = data.select_one('td:nth-child(2)').text.strip()
 
-        send_data = f"제목 : {title}\n날짜 : {date}\n\n링크 : {link}"
-        print(send_data)
-
+        no = data.select_one("td.no > span.mini_eng")
+        if no is not None:
+            number = int(no.text.strip())
+            if(number > 68):
+                send = True
+                for s in send_list:
+                    if(s['title'] == title):
+                        print("보낸적 있음")
+                        send = False
+                if send :
+                    text = f"제목 : {title}\n날짜 : {date}\n\n링크 : {link}"
+                    r = send_to_kakao(text)
+                    print(r.text)
+                    send_list.append({
+                        "title" : title,
+                        "link" : link,
+                        "number" : number,
+                        "date" : date
+                    })
 
 if __name__ == "__main__":
+    # while True:
+    #     get_data()
+    #     time.sleep(60)
     get_data()
