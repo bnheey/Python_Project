62eb535c755f6c31027f24d801979a6c3475611c
Heeeon <bbjoite09@naver.com>
2021-06-11 02:10:59 +0900

    grid - sticky  속성(North, East, West, South)

--- tkinter_project/tkinter_basic/14_grid.py
+++ tkinter_project/tkinter_basic/14_grid.py
@@ -19,10 +19,10 @@ btn_f17 = Button(root, text="F17")
 btn_f18 = Button(root, text="F18")
 btn_f19 = Button(root, text="F19")
 
-btn_f16.grid(row=0, column=0)
-btn_f17.grid(row=0, column=1)
-btn_f18.grid(row=0, column=2)
-btn_f19.grid(row=0, column=3)
+btn_f16.grid(row=0, column=0, sticky=N+E+W+S)
+btn_f17.grid(row=0, column=1, sticky=N+E+W+S)
+btn_f18.grid(row=0, column=2, sticky=N+E+W+S)
+btn_f19.grid(row=0, column=3, sticky=N+E+W+S)
 
 # clear 줄
 btn_clear = Button(root, text="clear")
@@ -30,10 +30,10 @@ btn_equal = Button(root, text="=")
 btn_div = Button(root, text="/")
 btn_mul = Button(root, text="*")
 
-btn_clear.grid(row=1, column=0)
-btn_equal.grid(row=1, column=1)
-btn_div.grid(row=1, column=2)
-btn_mul.grid(row=1, column=3)
+btn_clear.grid(row=1, column=0, sticky=N+E+W+S)
+btn_equal.grid(row=1, column=1, sticky=N+E+W+S)
+btn_div.grid(row=1, column=2, sticky=N+E+W+S)
+btn_mul.grid(row=1, column=3, sticky=N+E+W+S)
 
 # 7 시작 줄
 btn_7 = Button(root, text="7")
@@ -41,21 +41,21 @@ btn_8 = Button(root, text="8")
 btn_9 = Button(root, text="9")
 btn_sub = Button(root, text="-")
 
-btn_7.grid(row=2, column=0)
-btn_8.grid(row=2, column=1)
-btn_9.grid(row=2, column=2)
-btn_sub.grid(row=2, column=3)
+btn_7.grid(row=2, column=0, sticky=N+E+W+S)
+btn_8.grid(row=2, column=1, sticky=N+E+W+S)
+btn_9.grid(row=2, column=2, sticky=N+E+W+S)
+btn_sub.grid(row=2, column=3, sticky=N+E+W+S)
 
 # 4 시작 줄
-btn_4 = Button(root, text="7")
-btn_5 = Button(root, text="8")
-btn_6 = Button(root, text="9")
+btn_4 = Button(root, text="4")
+btn_5 = Button(root, text="5")
+btn_6 = Button(root, text="6")
 btn_add = Button(root, text="+")
 
-btn_4.grid(row=3, column=0)
-btn_5.grid(row=3, column=1)
-btn_6.grid(row=3, column=2)
-btn_add.grid(row=3, column=3)
+btn_4.grid(row=3, column=0, sticky=N+E+W+S)
+btn_5.grid(row=3, column=1, sticky=N+E+W+S)
+btn_6.grid(row=3, column=2, sticky=N+E+W+S)
+btn_add.grid(row=3, column=3, sticky=N+E+W+S)
 
 # 1 시작 줄
 btn_1 = Button(root, text="1")
@@ -63,16 +63,16 @@ btn_2 = Button(root, text="2")
 btn_3 = Button(root, text="3")
 btn_enter = Button(root, text="enter")  # 세로로 합쳐짐
 
-btn_4.grid(row=4, column=0)
-btn_5.grid(row=4, column=1)
-btn_6.grid(row=4, column=2)
-btn_enter.grid(row=4, column=3, rowspan=2)  # rowspan -> 현재 위치로 부터 아래쪽으로 몇줄을 더함
+btn_1.grid(row=4, column=0, sticky=N+E+W+S)
+btn_2.grid(row=4, column=1, sticky=N+E+W+S)
+btn_3.grid(row=4, column=2, sticky=N+E+W+S)
+btn_enter.grid(row=4, column=3, rowspan=2, sticky=N+E+W+S)  # rowspan -> 현재 위치로 부터 아래쪽으로 몇줄을 더함
 
 # 0 시작줄
 btn_0 = Button(root, text="0")  # 가로로 합쳐짐
 btn_point = Button(root, text=".")
 
-btn_0.grid(row=5, column=0, columnspan=2)  # columnspan -> 현재 위치로 부터 오른쪽으로 몇줄을 더함
-btn_point.grid(row=5, column=2)
+btn_0.grid(row=5, column=0, columnspan=2, sticky=N+E+W+S)  # columnspan -> 현재 위치로 부터 오른쪽으로 몇줄을 더함
+btn_point.grid(row=5, column=2, sticky=N+E+W+S)
 
 root.mainloop()
