abf40e828b7414a32a68289c1af2032e05b1b5cb
Heeeon <bbjoite09@naver.com>
2021-06-15 23:01:59 +0900

    메뉴 동작(open_file, save_file 함수 정의)

--- tkinter_project/memo_project/memo.py
+++ tkinter_project/memo_project/memo.py
@@ -13,30 +13,35 @@ tkinter를 이용한 메모장 프로그램 만들기
 6. 프로그램 크기, 위치는 자유롭게 하되 크기 조정이 가능해야함
 7. 본문 우측에 상하 스크롤바 넣기
 '''
-
+import os.path
 from tkinter import *
 
 root = Tk()
-root.title("제목없음 - windows 메모장")
+root.title("제목없음 - windows 메모장") # 조건 1 - OK
 root.geometry("640x480")  # 크기 지정
 
 root.resizable(True, True)  # 조건 6 - OK 크기조정이 가능하도록 설정
 
 # 화면 전체에 텍스트 엔트리 배치
-txt_box = Text(root, width=root.winfo_screenwidth(), height=root.winfo_screenheight())
-txt_box.pack()
+txt_box = Text(root)
+txt_box.pack(side="left", fill="both", expand="True")
 
 # 메뉴바 생성
 menu = Menu(root)
 
-# 파일 메뉴 함수 - 열기, 저장
+# 파일 메뉴 함수 - 열기, 저장 -> 조건 3 - OK
 def open_file():
-    return 0
+    if os.path.isfile("mynote.txt") :
+        with open("mynote.txt", "r", encoding="utf8") as file :
+            txt_box.delete("1.0", END)
+            txt_box.insert(END, file.read())
 
 def save_file():
-    return 0
+    with open("mynote.txt", "w", encoding="utf8") as file:
+        file.write(txt_box.get("1.0", END))
 
-# 파일 메뉴 배치
+
+# 파일 메뉴 배치 -> 조건 2 - OK
 menu_file = Menu(menu, tearoff=0)
 menu_file.add_command(label="열기(O)", command=open_file)
 menu_file.add_command(label="저장(S)", command=save_file)
@@ -45,9 +50,6 @@ menu_file.add_separator()
 menu_file.add_command(label="끝내기(X)", command=root.quit)
 
 menu.add_cascade(label="파일(F)", menu=menu_file)
-
-
-
 
 # 기타 메뉴
 menu.add_cascade(label="편집(E)")
