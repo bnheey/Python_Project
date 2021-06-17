32667c092dbdc725236ee13e527025d535bb6c09
Heeeon <bbjoite09@naver.com>
2021-06-17 23:05:59 +0900

    리스트 프레임 생성(파일 추가할 리스트 박스 생성)

--- tkinter_project/image_project/comb_image.py
+++ tkinter_project/image_project/comb_image.py
@@ -36,5 +36,17 @@ btn_add_file.pack(side="left")
 btn_del_file = Button(file_frame, padx=5, pady=5, width=12, text="선택삭제")
 btn_del_file.pack(side="right")
 
+# 리스트 프레임
+list_frame=Frame(root)
+list_frame.pack(fill="both")
+
+scrollbar = Scrollbar(list_frame)
+scrollbar.pack(side="right", fill="y")
+
+list_file = Listbox(list_frame, selectmode="extended", height=15, yscrollcommand=scrollbar.set)
+list_file.pack(side="left", fill="both", expand=True)
+scrollbar.config(command=list_file.yview)
+
+
 root.resizable(False, False)
 root.mainloop()
