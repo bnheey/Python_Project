0be6b62dc532b7e908b62adb04278c6b6ccf082d
Heeeon <bbjoite09@naver.com>
2021-06-17 23:19:21 +0900

    옵션 프레임 생성(가로 넓이, 간격 옵션, 파일 포맷 콤보 생성)

--- tkinter_project/image_project/comb_image.py
+++ tkinter_project/image_project/comb_image.py
@@ -22,6 +22,7 @@
 
 
 from tkinter import *
+import tkinter.ttk as ttk
 
 root = Tk()
 root.title("Combine Image Program") # 조건 1 - OK
@@ -57,6 +58,39 @@ txt_dest_path.pack(side="left", fill="x", expand=True, ipady=3)
 btn_dest_path = Button(path_frame, text="찾아보기", width=10)
 btn_dest_path.pack(side="right")
 
+# 옵션 프레임
+frame_option = LabelFrame(root, text="옵션")
+frame_option.pack()
+
+# 1. 가로 넓이 옵션
+# 가로 넓이 레이블
+lbl_width = Label(frame_option, text="가로넓이", width=8)
+lbl_width.pack(side="left")
+# 가로 넓이 콤보
+opt_width = ["원본유지", "1024", "800", "640"]
+cmb_width = ttk.Combobox(frame_option, state="readonly", values=opt_width, width=10)
+cmb_width.current(0)
+cmb_width.pack(side="left")
+
+# 2. 간격 옵션
+# 간격 옵션 레이블
+lbl_space = Label(frame_option, text="간격", width=8)
+lbl_space.pack(side="left")
+# 간격 옵션 콤보
+opt_space = ["없음", "좁게", "보통", "넓게"]
+cmb_space = ttk.Combobox(frame_option, state="readonly", values=opt_space, width=10)
+cmb_space.current(0)
+cmb_space.pack(side="left")
+
+# 3. 파일 포맷 옵션
+# 파일 포맷 레이블
+lbl_format = Label(frame_option, text="포맷", width=8)
+lbl_format.pack(side="left")
+# 파일 포맷 콤보
+opt_format = ["PNG", "JPG", "BMP"]
+cmb_format = ttk.Combobox(frame_option, state="readonly", values=opt_format, width=10)
+cmb_format.current(0)
+cmb_format.pack(side="left")
 
 
 root.resizable(False, False)
